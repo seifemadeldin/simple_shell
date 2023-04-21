03625039a0cf6c99e9dc6f5d3b47fad72c013a0a
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-04-21 14:02:16 +0200

    Update GetLine.c

--- GetLine.c
+++ GetLine.c
@@ -1,16 +1,16 @@
 #include "shell.h"
 /**
- * _getline_command - print "#cisfun$ " and wait for the user type something.
+ * GetLine - print "#cisfun$ " and wait for the user type something.
  * Return: line of string input for user
  */
 
 char *Getline(void) {
-    char *lineptr = NULL;
+	char *lineptr = NULL;
 	size_t charter_user = 0;
 
 	if (isatty(STDIN_FILENO)) {
 		write(STDOUT_FILENO, "$ ", 2);
-    }
+	}
 
 	if (getline(&lineptr, &charter_user, stdin) == -1) {
 		free(lineptr);
