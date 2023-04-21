12e154ddf356e12eb2dedf275d7090306e52a43e
seifemad <seifemad100@gmail.com>
2023-04-21 13:46:51 +0200

    Create GetLine.c

--- main.c
+++ main.c
@@ -1,2 +1,2 @@
-#include "main.h"
+#include "shell.h"
 
---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+#define _GNU_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+
+
+/*GetLine.c*/
+char *Getline(void);
+
+
+#endif /* SHELL_H */
