22651110c0c4fed7a3ff8f6a1fdb1dc945d1b779
Seif Emad El-Din <88642755+seifemadeldin@users.noreply.github.com>
2023-04-27 15:03:18 +0200

    Update execmd.c

--- execmd.c
+++ execmd.c
@@ -6,21 +6,21 @@
 
 void execmd(char **argv)
 {
-    char *command = NULL;
-    char *actual_command = NULL;
+	char *command = NULL;
+	char *actual_command = NULL;
 
-    if (argv)
-    {
-        /*get command from argv*/
-        command = argv[0];
+	if (argv)
+	{
+		/*get command from argv*/
+		command = argv[0];
 
-        /*generate the path to this command*/
-        actual_command = get_location(command);
+		/*generate the path to this command*/
+		actual_command = get_location(command);
 
-        /*execute the command*/
-        if (execve(actual_command, argv, NULL) == -1)
-        {
-            perror("Error: ");
-        }
-    }
+		/*execute the command*/
+		if (execve(actual_command, argv, NULL) == -1)
+		{
+			perror("Error: ");
+		}
+	}
 }
