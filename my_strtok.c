b0900c3ee09a8d64791a4a8aa1927299061c3c00
root <math.tadesse@gmail.com>
2023-05-18 01:09:27 -0700

    some updates

--- my_strtok.c
+++ my_strtok.c
@@ -1,32 +1,22 @@
 #include "shell.h"
 
-char *my_strchr(const char *s, int c);
-size_t my_strspn(const char *s1, const char *s2);
-size_t my_strcspn(const char *s1, const char *s2);
-
 /**
- * my_strtok - breaks the string s1 into tokens and null-terminates them.
- * Delimiter-Characters at the beginning and end
- *of str are skipped. On each subsequent call delim may change.
- * @str: string to tokenize
- * @delim: string with the character that delimit srt.
- * Return: the first/next token if possible, a null-pointer otherwise.
+ * my_strchr - locates the ﬁrst occurrence of c (converted to a char) in the
+ * string pointed to by s. The terminating null character is considered to be
+ * part of the string.
+ * @s: string
+ * @c: character
+ * Return: a pointer to the located character, or a null pointer
+ * if the character does not occur in the string.
  **/
-char *my_strtok(char *str, const char *delim)
+char *my_strchr(const char *s, int c)
 {
-	static char *p;
-
-	if (str)
-		p = str;
-	else if (!p)
-		return (0);
-	str = p + my_strspn(p, delim);
-	p = str + my_strcspn(str, delim);
-	if (p == str)
-		return (p = 0);
-	p = *p ? *p = 0, p + 1 : 0;
-	return (str);
+	while (*s != (char)c)
+		if (!*s++)
+			return (0);
+	return ((char *)s);
 }
+
 /**
  * my_strcspn - computes the length of the maximum segment of the string
  * pointed to by s1which consists entirely of characters not from the
@@ -47,6 +37,7 @@ size_t my_strcspn(const char *s1, const char *s2)
 	}
 	return (ret);
 }
+
 /**
  * my_strspn - computes the length of the maximum initial segment of the string
  * pointed to by s1 which consists entirely of characters from the string
@@ -63,19 +54,27 @@ size_t my_strspn(const char *s1, const char *s2)
 		ret++;
 	return (ret);
 }
+
 /**
- * my_strchr - locates the ﬁrst occurrence of c (converted to a char) in the
- * string pointed to by s. The terminating null character is considered to be
- * part of the string.
- * @s: string
- * @c: character
- * Return: a pointer to the located character, or a null pointer
- * if the character does not occur in the string.
+ * my_strtok - breaks the string s1 into tokens and make null-terminated
+ * Delimiter-Characters at the beginning and end
+ * of str are skipped. On each subsequent call delim may change.
+ * @str: string to tokenize
+ * @delim: string with the character that delimit srt.
+ * Return: the first/next token if possible, a null-pointer otherwise.
  **/
-char *my_strchr(const char *s, int c)
+char *my_strtok(char *str, const char *delim)
 {
-	while (*s != (char)c)
-		if (!*s++)
-			return (0);
-	return ((char *)s);
+	static char *p;
+
+	if (str)
+		p = str;
+	else if (!p)
+		return (0);
+	str = p + my_strspn(p, delim);
+	p = str + my_strcspn(str, delim);
+	if (p == str)
+		return (p = 0);
+	p = *p ? *p = 0, p + 1 : 0;
+	return (str);
 }
